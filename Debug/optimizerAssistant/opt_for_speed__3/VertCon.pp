7491bfeadc11962544ee4418d50b452a5e4f2ddb
Cealestus <meisler@live.ca>
2016-02-19 13:51:27 -0600

    Preliminary Work
    3 LEDs now rotate through turning off and on, speed undetermined. Using
    P2.7, P2.6, P2.4

---
+++ Debug/optimizerAssistant/opt_for_speed__3/VertCon.pp
@@ -0,0 +1,23 @@
+# FIXED
+
+VertCon.obj: ../VertCon.c
+VertCon.obj: C:/ti/ccsv6/ccs_base/arm/include/msp.h
+VertCon.obj: C:/ti/ccsv6/ccs_base/arm/include/msp432p401r.h
+VertCon.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.7/include/stdint.h
+VertCon.obj: C:/ti/ccsv6/ccs_base/arm/include/msp_compatibility.h
+VertCon.obj: C:/ti/ccsv6/ccs_base/arm/include/CMSIS/cmsis_ccs.h
+VertCon.obj: C:/ti/ccsv6/ccs_base/arm/include/CMSIS/core_cm4.h
+VertCon.obj: C:/ti/ccsv6/ccs_base/arm/include/CMSIS/core_cmInstr.h
+VertCon.obj: C:/ti/ccsv6/ccs_base/arm/include/CMSIS/core_cmFunc.h
+VertCon.obj: C:/ti/ccsv6/ccs_base/arm/include/CMSIS/core_cmSimd.h
+
+../VertCon.c: 
+C:/ti/ccsv6/ccs_base/arm/include/msp.h: 
+C:/ti/ccsv6/ccs_base/arm/include/msp432p401r.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.7/include/stdint.h: 
+C:/ti/ccsv6/ccs_base/arm/include/msp_compatibility.h: 
+C:/ti/ccsv6/ccs_base/arm/include/CMSIS/cmsis_ccs.h: 
+C:/ti/ccsv6/ccs_base/arm/include/CMSIS/core_cm4.h: 
+C:/ti/ccsv6/ccs_base/arm/include/CMSIS/core_cmInstr.h: 
+C:/ti/ccsv6/ccs_base/arm/include/CMSIS/core_cmFunc.h: 
+C:/ti/ccsv6/ccs_base/arm/include/CMSIS/core_cmSimd.h: 
--- VertCon.c
+++ VertCon.c
@@ -6,9 +6,41 @@
 
 #include "msp.h"
 
+volatile int counter;
+
+void init(void){
+	counter = 0;
+}
+
 void main(void)
 {
 	
     WDTCTL = WDTPW | WDTHOLD;           // Stop watchdog timer
+    init();
+    P2DIR |= (BIT7 | BIT6 | BIT4);
+    P2OUT &= ~(BIT7 | BIT6 | BIT4);
+
+
+    while(1){
+    	int i;
+    	if(counter == 2){
+    		counter = 0;
+    	} else{
+    		counter++;
+    	}
+    	P2OUT &= ~(BIT7 | BIT6 | BIT4);
+    	if(counter == 0){
+    		P2OUT |= BIT4;
+    	} else if(counter == 1){
+    		P2OUT |= BIT6;
+    	} else if(counter == 2){
+    		P2OUT |= BIT7;
+    	} else{
+
+    	}
+    	for(i=1000000; i>0; i--){         // Delay
+
+    	}
+    }
 	
 }
