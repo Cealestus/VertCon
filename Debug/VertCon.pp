53d740b219f4a08a06f0a71f320cfeba0398398e
mazloum91 <mazloum91@gmail.com>
2016-02-26 18:34:46 -0600

    Got IR light to blink. Trying to get feedback to an LED to detect if IR sensor detected light

--- Debug/VertCon.pp
+++ Debug/VertCon.pp
@@ -10,6 +10,41 @@ VertCon.obj: C:/ti/ccsv6/ccs_base/arm/include/CMSIS/core_cm4.h
 VertCon.obj: C:/ti/ccsv6/ccs_base/arm/include/CMSIS/core_cmInstr.h
 VertCon.obj: C:/ti/ccsv6/ccs_base/arm/include/CMSIS/core_cmFunc.h
 VertCon.obj: C:/ti/ccsv6/ccs_base/arm/include/CMSIS/core_cmSimd.h
+VertCon.obj: C:/Users/Naveed/workspace_v6_1/MSP432_DriverLib_2_20_00_08/driverlib/MSP432P4xx/driverlib.h
+VertCon.obj: C:/Users/Naveed/workspace_v6_1/MSP432_DriverLib_2_20_00_08/driverlib/MSP432P4xx/adc14.h
+VertCon.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdbool.h
+VertCon.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/yvals.h
+VertCon.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdarg.h
+VertCon.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/linkage.h
+VertCon.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/_lock.h
+VertCon.obj: C:/Users/Naveed/workspace_v6_1/MSP432_DriverLib_2_20_00_08/driverlib/MSP432P4xx/aes256.h
+VertCon.obj: C:/Users/Naveed/workspace_v6_1/MSP432_DriverLib_2_20_00_08/driverlib/MSP432P4xx/comp_e.h
+VertCon.obj: C:/Users/Naveed/workspace_v6_1/MSP432_DriverLib_2_20_00_08/driverlib/MSP432P4xx/cpu.h
+VertCon.obj: C:/Users/Naveed/workspace_v6_1/MSP432_DriverLib_2_20_00_08/driverlib/MSP432P4xx/crc32.h
+VertCon.obj: C:/Users/Naveed/workspace_v6_1/MSP432_DriverLib_2_20_00_08/driverlib/MSP432P4xx/cs.h
+VertCon.obj: C:/Users/Naveed/workspace_v6_1/MSP432_DriverLib_2_20_00_08/driverlib/MSP432P4xx/dma.h
+VertCon.obj: C:/Users/Naveed/workspace_v6_1/MSP432_DriverLib_2_20_00_08/driverlib/MSP432P4xx/eusci.h
+VertCon.obj: C:/Users/Naveed/workspace_v6_1/MSP432_DriverLib_2_20_00_08/driverlib/MSP432P4xx/flash.h
+VertCon.obj: C:/Users/Naveed/workspace_v6_1/MSP432_DriverLib_2_20_00_08/driverlib/MSP432P4xx/fpu.h
+VertCon.obj: C:/Users/Naveed/workspace_v6_1/MSP432_DriverLib_2_20_00_08/driverlib/MSP432P4xx/gpio.h
+VertCon.obj: C:/Users/Naveed/workspace_v6_1/MSP432_DriverLib_2_20_00_08/driverlib/MSP432P4xx/i2c.h
+VertCon.obj: C:/Users/Naveed/workspace_v6_1/MSP432_DriverLib_2_20_00_08/driverlib/MSP432P4xx/interrupt.h
+VertCon.obj: C:/Users/Naveed/workspace_v6_1/MSP432_DriverLib_2_20_00_08/driverlib/MSP432P4xx/mpu.h
+VertCon.obj: C:/Users/Naveed/workspace_v6_1/MSP432_DriverLib_2_20_00_08/driverlib/MSP432P4xx/pcm.h
+VertCon.obj: C:/Users/Naveed/workspace_v6_1/MSP432_DriverLib_2_20_00_08/driverlib/MSP432P4xx/pmap.h
+VertCon.obj: C:/Users/Naveed/workspace_v6_1/MSP432_DriverLib_2_20_00_08/driverlib/MSP432P4xx/pss.h
+VertCon.obj: C:/Users/Naveed/workspace_v6_1/MSP432_DriverLib_2_20_00_08/driverlib/MSP432P4xx/ref_a.h
+VertCon.obj: C:/Users/Naveed/workspace_v6_1/MSP432_DriverLib_2_20_00_08/driverlib/MSP432P4xx/reset.h
+VertCon.obj: C:/Users/Naveed/workspace_v6_1/MSP432_DriverLib_2_20_00_08/driverlib/MSP432P4xx/rom.h
+VertCon.obj: C:/Users/Naveed/workspace_v6_1/MSP432_DriverLib_2_20_00_08/driverlib/MSP432P4xx/rom_map.h
+VertCon.obj: C:/Users/Naveed/workspace_v6_1/MSP432_DriverLib_2_20_00_08/driverlib/MSP432P4xx/rtc_c.h
+VertCon.obj: C:/Users/Naveed/workspace_v6_1/MSP432_DriverLib_2_20_00_08/driverlib/MSP432P4xx/spi.h
+VertCon.obj: C:/Users/Naveed/workspace_v6_1/MSP432_DriverLib_2_20_00_08/driverlib/MSP432P4xx/sysctl.h
+VertCon.obj: C:/Users/Naveed/workspace_v6_1/MSP432_DriverLib_2_20_00_08/driverlib/MSP432P4xx/systick.h
+VertCon.obj: C:/Users/Naveed/workspace_v6_1/MSP432_DriverLib_2_20_00_08/driverlib/MSP432P4xx/timer32.h
+VertCon.obj: C:/Users/Naveed/workspace_v6_1/MSP432_DriverLib_2_20_00_08/driverlib/MSP432P4xx/timer_a.h
+VertCon.obj: C:/Users/Naveed/workspace_v6_1/MSP432_DriverLib_2_20_00_08/driverlib/MSP432P4xx/uart.h
+VertCon.obj: C:/Users/Naveed/workspace_v6_1/MSP432_DriverLib_2_20_00_08/driverlib/MSP432P4xx/wdt_a.h
 
 ../VertCon.c: 
 C:/ti/ccsv6/ccs_base/arm/include/msp.h: 
@@ -21,3 +56,38 @@ C:/ti/ccsv6/ccs_base/arm/include/CMSIS/core_cm4.h:
 C:/ti/ccsv6/ccs_base/arm/include/CMSIS/core_cmInstr.h: 
 C:/ti/ccsv6/ccs_base/arm/include/CMSIS/core_cmFunc.h: 
 C:/ti/ccsv6/ccs_base/arm/include/CMSIS/core_cmSimd.h: 
+C:/Users/Naveed/workspace_v6_1/MSP432_DriverLib_2_20_00_08/driverlib/MSP432P4xx/driverlib.h: 
+C:/Users/Naveed/workspace_v6_1/MSP432_DriverLib_2_20_00_08/driverlib/MSP432P4xx/adc14.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdbool.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/yvals.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdarg.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/linkage.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/_lock.h: 
+C:/Users/Naveed/workspace_v6_1/MSP432_DriverLib_2_20_00_08/driverlib/MSP432P4xx/aes256.h: 
+C:/Users/Naveed/workspace_v6_1/MSP432_DriverLib_2_20_00_08/driverlib/MSP432P4xx/comp_e.h: 
+C:/Users/Naveed/workspace_v6_1/MSP432_DriverLib_2_20_00_08/driverlib/MSP432P4xx/cpu.h: 
+C:/Users/Naveed/workspace_v6_1/MSP432_DriverLib_2_20_00_08/driverlib/MSP432P4xx/crc32.h: 
+C:/Users/Naveed/workspace_v6_1/MSP432_DriverLib_2_20_00_08/driverlib/MSP432P4xx/cs.h: 
+C:/Users/Naveed/workspace_v6_1/MSP432_DriverLib_2_20_00_08/driverlib/MSP432P4xx/dma.h: 
+C:/Users/Naveed/workspace_v6_1/MSP432_DriverLib_2_20_00_08/driverlib/MSP432P4xx/eusci.h: 
+C:/Users/Naveed/workspace_v6_1/MSP432_DriverLib_2_20_00_08/driverlib/MSP432P4xx/flash.h: 
+C:/Users/Naveed/workspace_v6_1/MSP432_DriverLib_2_20_00_08/driverlib/MSP432P4xx/fpu.h: 
+C:/Users/Naveed/workspace_v6_1/MSP432_DriverLib_2_20_00_08/driverlib/MSP432P4xx/gpio.h: 
+C:/Users/Naveed/workspace_v6_1/MSP432_DriverLib_2_20_00_08/driverlib/MSP432P4xx/i2c.h: 
+C:/Users/Naveed/workspace_v6_1/MSP432_DriverLib_2_20_00_08/driverlib/MSP432P4xx/interrupt.h: 
+C:/Users/Naveed/workspace_v6_1/MSP432_DriverLib_2_20_00_08/driverlib/MSP432P4xx/mpu.h: 
+C:/Users/Naveed/workspace_v6_1/MSP432_DriverLib_2_20_00_08/driverlib/MSP432P4xx/pcm.h: 
+C:/Users/Naveed/workspace_v6_1/MSP432_DriverLib_2_20_00_08/driverlib/MSP432P4xx/pmap.h: 
+C:/Users/Naveed/workspace_v6_1/MSP432_DriverLib_2_20_00_08/driverlib/MSP432P4xx/pss.h: 
+C:/Users/Naveed/workspace_v6_1/MSP432_DriverLib_2_20_00_08/driverlib/MSP432P4xx/ref_a.h: 
+C:/Users/Naveed/workspace_v6_1/MSP432_DriverLib_2_20_00_08/driverlib/MSP432P4xx/reset.h: 
+C:/Users/Naveed/workspace_v6_1/MSP432_DriverLib_2_20_00_08/driverlib/MSP432P4xx/rom.h: 
+C:/Users/Naveed/workspace_v6_1/MSP432_DriverLib_2_20_00_08/driverlib/MSP432P4xx/rom_map.h: 
+C:/Users/Naveed/workspace_v6_1/MSP432_DriverLib_2_20_00_08/driverlib/MSP432P4xx/rtc_c.h: 
+C:/Users/Naveed/workspace_v6_1/MSP432_DriverLib_2_20_00_08/driverlib/MSP432P4xx/spi.h: 
+C:/Users/Naveed/workspace_v6_1/MSP432_DriverLib_2_20_00_08/driverlib/MSP432P4xx/sysctl.h: 
+C:/Users/Naveed/workspace_v6_1/MSP432_DriverLib_2_20_00_08/driverlib/MSP432P4xx/systick.h: 
+C:/Users/Naveed/workspace_v6_1/MSP432_DriverLib_2_20_00_08/driverlib/MSP432P4xx/timer32.h: 
+C:/Users/Naveed/workspace_v6_1/MSP432_DriverLib_2_20_00_08/driverlib/MSP432P4xx/timer_a.h: 
+C:/Users/Naveed/workspace_v6_1/MSP432_DriverLib_2_20_00_08/driverlib/MSP432P4xx/uart.h: 
+C:/Users/Naveed/workspace_v6_1/MSP432_DriverLib_2_20_00_08/driverlib/MSP432P4xx/wdt_a.h: 
--- VertCon.c
+++ VertCon.c
@@ -13,33 +13,48 @@ void init(void){
 	counter = 0;
 }
 
+void check_light(void){
+	if(GPIO_INPUT_PIN_LOW == GPIO_getInputPinValue(GPIO_PORT_P1, GPIO_PIN5)){
+		P2OUT |= BIT7;
+	}
+	else{
+		P2OUT &= ~BIT7;
+	}
+
+}
 void main(void)
 {
 	
     WDTCTL = WDTPW | WDTHOLD;           // Stop watchdog timer
     init();
-    P2DIR |= (BIT7 | BIT6 | BIT4);
-    P2OUT &= ~(BIT7 | BIT6 | BIT4);
-
+    GPIO_setAsOutputPin(GPIO_PORT_P2,GPIO_PIN7);
+    GPIO_setAsOutputPin(GPIO_PORT_P2,GPIO_PIN6);
+	GPIO_setAsOutputPin(GPIO_PORT_P2,GPIO_PIN4);
+	GPIO_setAsInputPin(GPIO_PORT_P1,GPIO_PIN5);
 
     while(1){
     	int i;
-    	if(counter == 2){
+    	if(counter == 1){
     		counter = 0;
     	} else{
     		counter++;
     	}
-    	P2OUT &= ~(BIT7 | BIT6 | BIT4);
+
     	if(counter == 0){
+    		P2OUT &= ~(BIT6);
     		P2OUT |= BIT4;
-    	} else if(counter == 1){
+        	check_light();
+
+    	}
+    	else if(counter == 1){
+    		P2OUT &= ~(BIT4);
     		P2OUT |= BIT6;
-    	} else if(counter == 2){
-    		P2OUT |= BIT7;
+        	check_light();
+
     	} else{
 
     	}
-    	for(i=1488000; i>0; i--);         // Delay
+    	for(i=1000000; i>0; i--);         // Delay
     }
 	
 }
