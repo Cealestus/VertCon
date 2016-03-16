f00d4e09379dbbe573a7330e43775484063da618
Matt <meisler@live.ca>
2016-03-16 12:51:55 -0600

    Uncommented sections related to BT communication

--- VertConClient1.py
+++ VertConClient1.py
@@ -6,8 +6,8 @@ import time
 
 serverMACAddress = 'B8:27:EB:FC:C1:76' #Found from using hciconfig
 port = 7
-#s = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
-#s.connect((serverMACAddress, port))
+s = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
+s.connect((serverMACAddress, port))
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(37, GPIO.IN)
 while 1:
@@ -15,7 +15,7 @@ while 1:
         time.sleep(.01)
     print('detected light')
     text = "Hello"
-    #s.send(bytes(text, 'UTF-8'))
+    s.send(bytes(text, 'UTF-8'))
     time.sleep(.5)
 s.close()
 GPIO.cleanup()
